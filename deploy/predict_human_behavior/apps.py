b92b32a41e263f42954db79b3fcfcf05b08481af
RaphaelHenin <raphael.henin@laposte.net>
2020-01-31 22:52:18 +0100

    Remove Django API

--- deploy/predict_human_behavior/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-class PredictHumanBehaviorConfig(AppConfig):
-    name = 'predict_human_behavior'
-    
-
-
--- deploy/predict_human_behavior/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls   import url
-from predict_human_behavior import views
-from django.urls import path
-
-urlpatterns = [
-    path('model/', views.get)
-]
